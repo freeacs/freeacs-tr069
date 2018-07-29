ccab634cbec4079b2b8fd8b0d0a984bc262da01a
Jarl André Hübenthal <jarl.andre@gmail.com>
2018-07-29 16:50:31 +0200

    forgot autoinc

--- src/main/scala/com/github/freeacs/repository/unitType/UnitTypeTable.scala
+++ src/main/scala/com/github/freeacs/repository/unitType/UnitTypeTable.scala
@@ -6,7 +6,7 @@ trait UnitTypeTable { this: Db =>
   import config.profile.api._
 
   class UnitTypes(tag: Tag) extends Table[UnitType](tag, "unit_type") {
-    def unitTypeId = column[Long]("UNIT_TYPE_ID", O.PrimaryKey)
+    def unitTypeId = column[Long]("UNIT_TYPE_ID", O.PrimaryKey, O.AutoInc)
     def unitTypeName = column[String]("UNIT_TYPE_NAME")
     def matcherId = column[String]("MATCHER_ID")
     def vendorName = column[String]("VENDOR_NAME")
