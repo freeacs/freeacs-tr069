7b0b8e19f89ea7ae3b284233fb8321b65485ee76
Jarl André Hübenthal <jarl.andre@gmail.com>
2018-07-31 11:27:19 +0200

    playing around with mutable state

--- src/main/scala/com/github/freeacs/session/SessionActorImpl.scala
+++ src/main/scala/com/github/freeacs/session/SessionActorImpl.scala
@@ -8,16 +8,18 @@ import scala.concurrent.Future
 
 class SessionActorImpl(user: String, services: Tr069Services) extends SessionActor {
 
-  private var requests = mutable.Map[SOAPRequest, Option[SOAPResponse]]()
+  private var requests: mutable.Map[SOAPRequest, Option[SOAPResponse]] = mutable.LinkedHashMap()
 
   def request(request: SOAPRequest): Future[Option[SOAPResponse]] = request match {
     case inform: InformRequest =>
       val response = Some(InformResponse())
-      requests.clear() // reset
+      requests = mutable.Map()
       requests.put(inform, response)
+      println(requests)
       Future.successful(response)
     case EmptyRequest =>
       requests.put(EmptyRequest, None)
+      println(requests)
       Future.successful(None)
   }
 }
