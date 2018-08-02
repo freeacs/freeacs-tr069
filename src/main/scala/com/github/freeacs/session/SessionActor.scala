2286d345ce9794f2a8bafe76a70525e3a2c5c5c3
Jarl André Hübenthal <jarl.andre@gmail.com>
2018-08-02 14:12:38 +0200

    changing to list from map

--- src/main/scala/com/github/freeacs/session/SessionActor.scala
+++ src/main/scala/com/github/freeacs/session/SessionActor.scala
@@ -5,7 +5,6 @@ import akka.pattern.pipe
 import com.github.freeacs.services.Tr069Services
 import com.github.freeacs.xml._
 
-import scala.collection.mutable
 import scala.concurrent.{ExecutionContext, Future}
 
 object SessionActor {
@@ -17,24 +16,24 @@ class SessionActor(user: String, services: Tr069Services)(implicit ec: Execution
 
   log.info("Created session actor for " + user)
 
-  private var requests: mutable.Map[SOAPRequest, Option[SOAPResponse]] = mutable.LinkedHashMap()
+  private var requests: List[(SOAPRequest, Option[SOAPResponse])] = List()
 
   override def receive: Receive = {
     case request: SOAPRequest =>
       request match {
         case inform: InformRequest =>
           val response = Some(InformResponse())
-          requests = mutable.Map()
-          requests.put(inform, response)
+          requests = List((request, response))
           pipe(Future.successful(response)) to sender
         case EmptyRequest =>
-          requests.put(EmptyRequest, None)
-          pipe(Future.successful(None)) to sender
+          requests = requests :+ (request, None)
+          log.info(requests.mkString(", "))
+          sender ! None
           self ! PoisonPill
       }
     case unknown =>
       log.error(s"Got unknown message $unknown")
-      sender ! PoisonPill
+      self ! PoisonPill
   }
 
 }
