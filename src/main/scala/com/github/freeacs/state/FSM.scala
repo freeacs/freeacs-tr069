13fb46d33ad0cc5978ed0745da55567c93fe48aa
Jarl André Hübenthal <jarl.andre@gmail.com>
2018-08-17 22:44:36 +0200

    remove replicateddata from FSM

--- src/main/scala/com/github/freeacs/state/FSM.scala
+++ src/main/scala/com/github/freeacs/state/FSM.scala
@@ -3,19 +3,14 @@
 
 package com.github.freeacs.state
 
-import akka.cluster.ddata.ReplicatedData
 import com.github.freeacs.xml.SOAPRequest
 
 import scala.concurrent.{ExecutionContext, Future}
 
-final case class FSM(currentState: State) extends ReplicatedData {
+final case class FSM(currentState: State) {
   def transition(
       request: SOAPRequest,
       transform: Transformation
   )(implicit ec: ExecutionContext): Future[TransformationResult] =
     transform(currentState, request)
-
-  override type T = FSM
-  override def merge(that: FSM): FSM =
-    FSM(that.currentState)
 }
