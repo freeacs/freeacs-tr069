5cc02c52803d5f63e75bae0597e90a0429924e86
Jarl André Hübenthal <jarl.andre@gmail.com>
2018-08-18 18:12:29 +0200

    removing unnecessary matching variables

--- src/main/scala/com/github/freeacs/session/SessionState.scala
+++ src/main/scala/com/github/freeacs/session/SessionState.scala
@@ -22,39 +22,31 @@ final case class SessionState(
 
   def transition(services: Tr069Services, request: SOAPRequest)(
       implicit ec: ExecutionContext
-  ): Future[(SessionState, SOAPResponse)] =
-    (this, request) match {
-      case (sessionState, request: InformRequest)
-          if sessionState.state == ExpectInformRequest =>
-        InformMethod.process(request, sessionState)
+  ): Future[(SessionState, SOAPResponse)] = request match {
+    case request: InformRequest if state == ExpectInformRequest =>
+      InformMethod.process(request, this)
 
-      case (sessionState, request: EmptyRequest)
-          if sessionState.state == ExpectEmptyRequest =>
-        EmptyMethod.process(request, sessionState)
+    case request: EmptyRequest if state == ExpectEmptyRequest =>
+      EmptyMethod.process(request, this)
 
-      case (sessionState, request: GetParameterNamesResponse)
-          if sessionState.state == ExpectGetParameterNamesResponse =>
-        GPNMethod.process(request, sessionState)
+    case request: GetParameterNamesResponse
+        if state == ExpectGetParameterNamesResponse =>
+      GPNMethod.process(request, this)
 
-      case (sessionState, request: GetParameterValuesResponse)
-          if sessionState.state == ExpectGetParameterValuesResponse =>
-        GPVMethod.process(request, sessionState)
+    case request: GetParameterValuesResponse
+        if state == ExpectGetParameterValuesResponse =>
+      GPVMethod.process(request, this)
 
-      case (sessionState, request: SetParameterValuesResponse)
-          if sessionState.state == ExpectSetParameterValuesResponse =>
-        SPVMethod.process(request, sessionState)
+    case request: SetParameterValuesResponse
+        if state == ExpectSetParameterValuesResponse =>
+      SPVMethod.process(request, this)
 
-      case (sessionState, _) =>
-        val newState =
-          if (sessionState.errorCount < 2)
-            sessionState.copy(
-              errorCount = (sessionState.errorCount + 1)
-            )
-          else
-            sessionState.copy(
-              errorCount = 0,
-              state = ExpectInformRequest
-            )
-        Future.successful((newState, InvalidRequest()))
-    }
+    case _ =>
+      val newState =
+        if (errorCount < 2)
+          copy(errorCount = (errorCount + 1))
+        else
+          copy(errorCount = 0, state = ExpectInformRequest)
+      Future.successful((newState, InvalidRequest()))
+  }
 }
