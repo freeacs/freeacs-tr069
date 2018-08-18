e5437038c8fb650085eebf110cd6f2c60cd03652
Jarl André Hübenthal <jarl.andre@gmail.com>
2018-08-18 14:44:28 +0200

    moving protocol to session state class

--- src/main/scala/com/github/freeacs/session/SessionState.scala
+++ src/main/scala/com/github/freeacs/session/SessionState.scala
@@ -2,8 +2,8 @@ package com.github.freeacs.session
 
 import akka.cluster.ddata.ReplicatedData
 import com.github.freeacs.services.Tr069Services
-import com.github.freeacs.session.SessionProtocol.TransformationResult
-import com.github.freeacs.xml.SOAPRequest
+import com.github.freeacs.xml._
+import org.slf4j.LoggerFactory
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -19,10 +19,105 @@ final case class SessionState(
   def merge(that: SessionState): SessionState =
     if (that.modified > this.modified) that else this
 
+  final case class Result(
+      response: SOAPResponse,
+      state: SessionState
+  )
+
+  val log = LoggerFactory.getLogger(getClass)
+
   def transition(
       services: Tr069Services,
       request: SOAPRequest
-  )(implicit ec: ExecutionContext): Future[TransformationResult] =
-    SessionProtocol.protocol(services).transform(this, request)
+  )(implicit ec: ExecutionContext): Future[Result] = (this, request) match {
+    case (
+        sessionState @ SessionState(_, _, ExpectInformRequest, _),
+        request: InformRequest
+        ) =>
+      log.info("Got INReq. Returning INRes. " + request.toString)
+      Future.successful(
+        Result(
+          InformResponse(),
+          sessionState.copy(state = ExpectEmptyRequest)
+        )
+      )
+
+    case (
+        sessionState @ SessionState(_, _, ExpectEmptyRequest, _),
+        EmptyRequest
+        ) =>
+      log.info("Got EM. Returning GPNReq.")
+      val response = GetParameterNamesRequest("InternetGatewayDevice.")
+      Future.successful(
+        Result(
+          response,
+          sessionState.copy(state = ExpectGetParameterNamesResponse)
+        )
+      )
+
+    case (
+        sessionState @ SessionState(_, _, ExpectGetParameterNamesResponse, _),
+        request: GetParameterNamesResponse
+        ) =>
+      log.info("Got GPNRes. Returning GPVReq. " + request.toString)
+      val response =
+        GetParameterValuesRequest(
+          Seq(("InternetGatewayDevice.ManagementServer.Username"))
+        )
+      Future.successful(
+        Result(
+          response,
+          sessionState.copy(state = ExpectGetParameterValuesResponse)
+        )
+      )
+
+    case (
+        sessionState @ SessionState(
+          _,
+          _,
+          ExpectGetParameterValuesResponse,
+          _
+        ),
+        request: GetParameterValuesResponse
+        ) =>
+      log.info("Got GPVRes. Returning SPVReq. " + request.toString)
+      val response = SetParameterValuesRequest()
+      Future.successful(
+        Result(
+          response,
+          sessionState.copy(state = ExpectSetParameterValuesResponse)
+        )
+      )
+
+    case (
+        sessionState @ SessionState(
+          _,
+          _,
+          ExpectSetParameterValuesResponse,
+          _
+        ),
+        request: SetParameterValuesResponse
+        ) =>
+      log.info("Got SPVRes. Returning EM. " + request.toString)
+      val response = EmptyResponse
+      Future.successful(
+        Result(
+          response,
+          sessionState.copy(state = ExpectInformRequest)
+        )
+      )
+
+    case (sessionState, req) =>
+      log.error(s"Got unexpected request $req in state ${sessionState.state}")
+      Future.successful(
+        Result(
+          InvalidRequest,
+          if (sessionState.errorCount < 2)
+            sessionState.copy(errorCount = (sessionState.errorCount + 1))
+          else
+            sessionState.copy(errorCount = 0, state = ExpectInformRequest)
+        )
+      )
+  }
 
 }
