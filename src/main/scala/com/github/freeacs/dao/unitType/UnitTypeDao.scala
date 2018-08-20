a286fab2cd9fc5dcbd8499015d385869d7b38664
Jarl André Hübenthal <jarl.andre@gmail.com>
2018-08-20 17:58:45 +0200

    porting to sql macro

--- src/main/scala/com/github/freeacs/dao/unitType/UnitTypeDao.scala
+++ src/main/scala/com/github/freeacs/dao/unitType/UnitTypeDao.scala
@@ -1,6 +1,7 @@
 package com.github.freeacs.dao.unitType
 
 import com.github.freeacs.dao.Dao
+import com.github.freeacs.domain.UnitType
 import slick.basic.DatabaseConfig
 import slick.jdbc.{GetResult, JdbcProfile}
 
@@ -9,8 +10,7 @@ import scala.concurrent.{ExecutionContext, Future}
 class UnitTypeDao(val config: DatabaseConfig[JdbcProfile])(
     implicit ec: ExecutionContext
 ) extends Dao
-    with UnitTypeTable
-    with UnitTypeParameterTable {
+    with UnitTypeTable {
 
   import config.profile.api._
 
@@ -44,34 +44,6 @@ class UnitTypeDao(val config: DatabaseConfig[JdbcProfile])(
   def getByName(name: String): Future[Option[UnitType]] =
     db.run(getByNameQuery(name))
 
-  def getByName2(
-      name: String
-  ): Future[Option[(UnitType, Seq[UnitTypeParameter])]] =
-    for {
-      unitType <- db.run(
-                   unitTypes.filter(_.unitTypeName === name).result.headOption
-                 )
-      params <- {
-        if (unitType.isDefined)
-          db.run(
-            unitTypeParameters
-              .filter(_.unitTypeId === unitType.get.unitTypeId)
-              .result
-          )
-        else
-          Future.successful(Seq.empty)
-      }
-    } yield
-      (
-        unitType.map(ut => {
-          (ut, params)
-        })
-      )
-
   def save(unitType: UnitType): Future[UnitType] =
-    db.run(
-      unitTypes returning unitTypes.map(_.unitTypeId)
-        into ((unitType, id) => unitType.copy(unitTypeId = Some(id)))
-        += unitType
-    )
+    ???
 }
